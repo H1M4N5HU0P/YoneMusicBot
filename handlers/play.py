b1e1dc00a3b0a48c75bc36b54a2cd7a702db8eed
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-05-29 20:27:39 +0530

    Add files via upload

---
+++ handlers/play.py
@@ -0,0 +1,68 @@
+from os import path
+
+from pyrogram import Client
+from pyrogram.types import Message, Voice
+
+from callsmusic import callsmusic, queues
+
+import converter
+from downloaders import youtube
+
+from config import BOT_NAME as bn, DURATION_LIMIT
+from helpers.filters import command, other_filters
+from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@Client.on_message(command("play") & other_filters)
+@errors
+async def play(_, message: Message):
+
+    lel = await message.reply("🔄 **Processing** PLEASE WAIT‼️")
+    sender_id = message.from_user.id
+    sender_name = message.from_user.first_name
+
+    keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Yone Music Bot🎶",
+                        url="https://t.me/YoneMusic_bot")
+                   
+                ]
+            ]
+        )
+
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"❌ {DURATION_LIMIT} minute(s) Too long audio! sorry i can't play‼️"
+            )
+
+        file_name = get_file_name(audio)
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name)) else file_name
+        )
+    elif url:
+        file_path = await converter.convert(youtube.download(url))
+    else:
+        return await lel.edit_text("")
+
+    if message.chat.id in callsmusic.pytgcalls.active_calls:
+        position = await queues.put(message.chat.id, file=file_path)
+        await lel.edit(f"#⃣ **Queued** at position {position}!")
+    else:
+        callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
+        await message.reply_photo(
+        photo="https://telegra.ph/file/fe77d94e2105721ce4353.jpg",
+        reply_markup=keyboard,
+        caption="Your song is **Playing** {}⚡".format(
+        message.from_user.mention()
+        ),
+    )
+        return await lel.delete()
---
+++ handlers/start.py
@@ -0,0 +1,101 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+@Client.on_message(
+    filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b>Hey {format(
+        message.from_user.mention)}!
+I am 𝐘𝐨𝐧𝐞 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭🎶🎸
+I can play songs in your group's VC [😉](https://telegra.ph/file/fe77d94e2105721ce4353.jpg)
+
+To listen songs add me to your group..
+
+And don't forgot to promote me with all rights!😉
+Otherwise I can't play songs!🥺👉👈
+
+Use the buttons below to know more about me..🔥
+ </b>""",
+      
+       
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Support Group❤️", url="https://t.me/KoraSupport",
+                    )
+                ],
+                [    
+                    InlineKeyboardButton(
+                        text="Commands🥰", callback_data="help"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "➕ Add To Your Group ➕", url="https://t.me/YoneMusic_bot?startgroup=true"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"🎶How To Use Yone Music Bot Explained Below Read Carefully!👇👇
+
+First Of All add this bot in your groups And make admin to work properly🎸
+
+The commands and there use is explained here:-
+
+1./saavn To search song on jio saavan and play the first result
+
+2./play song name To search the song on Youtube and play the first matching result(you can play yt link too).
+
+3./song Reply this in response to a any telegram audio file it will be played
+
+4./skip to skip current song
+
+5./end to stop the streaming of song
+
+6./pause to pause the strea
+
+7./resume to resume the playback.
+
+8.Inline search is also supported.
+
+📌Note= Sometime it works without admin rights but sometime it doesn't works so make admin to both the bots to work properly🤗
+
+Support Group:- @KoraSupport"
