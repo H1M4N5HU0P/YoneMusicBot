6f2e9b576b2ef26a4a20b470399883c7fa85f671
H1M4N5HU0P <82947455+H1M4N5HU0P@users.noreply.github.com>
2021-06-14 23:59:03 +0530

    Add files via upload

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @Yonemusic.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: Yonemusic, message: Message):
-  await Yonemusic.send_message(message.chat.id,"Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n")
+  await Yonemusic.send_message(message.chat.id,"Hi there, This is a music assistant service .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n ⚠️ Disclamer: If you are sending a message here it means admin will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n Add @YoneMusic_bot And Me to your group make us admin and enjoy vc music!!😉")
   return                        
